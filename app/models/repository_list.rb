9d1efd4255e851b0e366c86a8ba5ae40c98e2d9c
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-03-05 07:57:23 -0500

    Not working

--- app/models/repository_list.rb
+++ app/models/repository_list.rb
@@ -1,14 +1,26 @@
 class RepositoryList
   attr_reader :repositories
 
+#  def initialize(username)
+#    results = HTTParty.get(
+#        "https://api.github.com/users/#{username}/repos?sort=updated",
+#        :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
+#                     "User-Agent" => "anyone"
+#                    }
+#    )
+#    @repositories = results.map {|r| Repository.new(r)}
+#  end
   def initialize(username)
-    results = HTTParty.get(
-        "https://api.github.com/users/#{username}/repos?sort=updated",
-        :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
-                     "User-Agent" => "anyone"
-                    }
+    Rails.cache.fetch("#{cache_key}/users", :expires_in => 2.hours) do
+      results = HTTParty.get(
+      "https://api.github.com/users/#{username}/repos?sort=updated",
+      :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
+      "User-Agent" => "anyone"
+                  }
     )
     @repositories = results.map {|r| Repository.new(r)}
+
+    end
   end
 
 end
